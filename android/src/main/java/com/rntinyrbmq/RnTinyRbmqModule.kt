52e9ff40ed704329c142d6fb963d4603ef5847d3
Anton Efremenkov <efremenkov.anton@gmail.com>
2020-11-17 08:30:29 +0300

    fix: added isConnected check

--- android/src/main/java/com/rntinyrbmq/RnTinyRbmqModule.kt
+++ android/src/main/java/com/rntinyrbmq/RnTinyRbmqModule.kt
@@ -6,6 +6,7 @@ import com.rabbitmq.client.*
 
 class RnTinyRbmqModule(val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
+    private var isConnected = false
     private var connection: Connection? = null
     private var channel: Channel? = null
     private var consumer: DefaultConsumer? = null
@@ -34,58 +35,63 @@ class RnTinyRbmqModule(val reactContext: ReactApplicationContext) : ReactContext
 
     @ReactMethod
     fun connect() {
-      this.connection = this.factory!!.newConnection()
-      this.channel = this.connection!!.createChannel()
-      this.consumer = object : DefaultConsumer(this.channel) {
-        override fun handleConsumeOk(consumerTag: String?) {
-          super.handleConsumeOk(consumerTag)
-          if (consumerTag != null) {
-            consumerTags.add(consumerTag)
-          }
-
-          val event = Arguments.createMap()
-          event.putString("name", "connected")
-          reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("RnTinyRbmqEvent", event)
-        }
-
-        override fun handleShutdownSignal(consumerTag: String?, sig: ShutdownSignalException?) {
-          super.handleShutdownSignal(consumerTag, sig)
-          val event = Arguments.createMap()
-          event.putString("name", "error")
-          event.putString("type", "disconnected")
-          reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("RnTinyRbmqEvent", event)
-
-          if (consumerTag != null) {
-            consumerTags.remove(consumerTag)
-          }
-        }
-
-        override fun handleCancel(consumerTag: String?) {
-          super.handleCancel(consumerTag)
-          val event = Arguments.createMap()
-          event.putString("name", "error")
-          event.putString("type", "disconnected")
-          reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("RnTinyRbmqEvent", event)
-
-          if (consumerTag != null) {
-            consumerTags.remove(consumerTag)
-          }
-        }
-
-        override fun handleDelivery(consumerTag: String?, envelope: Envelope?, properties: AMQP.BasicProperties?, body: ByteArray?) {
-          val event = Arguments.createMap()
-
-          if (body != null) {
-            event.putString("name", "message")
-            event.putString("message", String(body, Charsets.UTF_8))
-            event.putString("routing_key", envelope?.routingKey)
-            event.putString("exchange", envelope?.exchange)
-            event.putString("consumer_tag", consumerTag)
-            if (envelope?.deliveryTag != null) {
-              event.putInt("delivery_tag", envelope.deliveryTag.toInt())
+      if (!this.isConnected) {
+        this.connection = this.factory!!.newConnection()
+        this.channel = this.connection!!.createChannel()
+        this.consumer = object : DefaultConsumer(this.channel) {
+          override fun handleConsumeOk(consumerTag: String?) {
+            super.handleConsumeOk(consumerTag)
+            if (consumerTag != null) {
+              consumerTags.add(consumerTag)
             }
 
+            this@RnTinyRbmqModule.isConnected = true
+            val event = Arguments.createMap()
+            event.putString("name", "connected")
             reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("RnTinyRbmqEvent", event)
+          }
+
+          override fun handleShutdownSignal(consumerTag: String?, sig: ShutdownSignalException?) {
+            super.handleShutdownSignal(consumerTag, sig)
+            this@RnTinyRbmqModule.isConnected = false
+            val event = Arguments.createMap()
+            event.putString("name", "error")
+            event.putString("type", "disconnected")
+            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("RnTinyRbmqEvent", event)
+
+            if (consumerTag != null) {
+              consumerTags.remove(consumerTag)
+            }
+          }
+
+          override fun handleCancel(consumerTag: String?) {
+            super.handleCancel(consumerTag)
+            this@RnTinyRbmqModule.isConnected = false
+            val event = Arguments.createMap()
+            event.putString("name", "error")
+            event.putString("type", "disconnected")
+            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("RnTinyRbmqEvent", event)
+
+            if (consumerTag != null) {
+              consumerTags.remove(consumerTag)
+            }
+          }
+
+          override fun handleDelivery(consumerTag: String?, envelope: Envelope?, properties: AMQP.BasicProperties?, body: ByteArray?) {
+            val event = Arguments.createMap()
+
+            if (body != null) {
+              event.putString("name", "message")
+              event.putString("message", String(body, Charsets.UTF_8))
+              event.putString("routing_key", envelope?.routingKey)
+              event.putString("exchange", envelope?.exchange)
+              event.putString("consumer_tag", consumerTag)
+              if (envelope?.deliveryTag != null) {
+                event.putInt("delivery_tag", envelope.deliveryTag.toInt())
+              }
+
+              reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("RnTinyRbmqEvent", event)
+            }
           }
         }
       }
@@ -93,21 +99,25 @@ class RnTinyRbmqModule(val reactContext: ReactApplicationContext) : ReactContext
 
     @ReactMethod
     fun basicConsume(queue: String) {
-      this.channel?.basicConsume(queue, true, this.consumer)
+      if (this.isConnected) {
+        this.channel?.basicConsume(queue, true, this.consumer)
+      }
     }
 
     @ReactMethod
     fun close() {
-      consumerTags.forEach {
-        this.channel?.basicCancel(it)
+      if (this.isConnected) {
+        consumerTags.forEach {
+          this.channel?.basicCancel(it)
+        }
+        consumerTags.clear()
+
+        this.connection?.close()
+
+        this.connection = null
+        this.channel = null
+        this.consumer = null
       }
-      consumerTags.clear()
-
-      this.connection?.close()
-
-      this.connection = null
-      this.channel = null
-      this.consumer = null
     }
 
 }
--- ios/RnTinyRbmq.m
+++ ios/RnTinyRbmq.m
@@ -1,6 +1,7 @@
 #import "RnTinyRbmq.h"
 
 @interface RnTinyRbmq ()
+    @property (nonatomic, readwrite) bool isConnected;
     @property (nonatomic, readwrite) NSDictionary *config;
     @property (nonatomic, readwrite) RMQConnection *connection;
     @property (nonatomic, readwrite) id<RMQChannel> channel;
@@ -16,43 +17,50 @@ RCT_EXPORT_METHOD(initialize:(NSDictionary *) config) {
 }
 
 RCT_EXPORT_METHOD(connect) {
-    RnTinyRbmqDelegateLogger *delegate = [[RnTinyRbmqDelegateLogger alloc] init];
-    NSString *protocol = @"amqp";
-    if ([self.config objectForKey:@"ssl"] != nil && [[self.config objectForKey:@"ssl"] boolValue]){
-        protocol = @"amqps";
-    }
-    
-    NSString *uri = [NSString stringWithFormat:@"%@://%@:%@@%@:%@/%@", protocol, self.config[@"username"], self.config[@"password"], self.config[@"host"], self.config[@"port"], self.config[@"virtualhost"]];
+    if (!self.isConnected) {
+        RnTinyRbmqDelegateLogger *delegate = [[RnTinyRbmqDelegateLogger alloc] init];
+        NSString *protocol = @"amqp";
+        if ([self.config objectForKey:@"ssl"] != nil && [[self.config objectForKey:@"ssl"] boolValue]){
+            protocol = @"amqps";
+        }
+        
+        NSString *uri = [NSString stringWithFormat:@"%@://%@:%@@%@:%@/%@", protocol, self.config[@"username"], self.config[@"password"], self.config[@"host"], self.config[@"port"], self.config[@"virtualhost"]];
 
-    self.connection = [[RMQConnection alloc] initWithUri:uri channelMax:@65535 frameMax:@(RMQFrameMax) heartbeat:@10 connectTimeout:@15 readTimeout:@30 writeTimeout:@30 syncTimeout:@10 delegate:delegate delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
-    
-    [self.connection start:^{
-        self.channel = [self.connection createChannel];
-        [RnTinyRbmqEventEmitter emitEventWithName:@"RnTinyRbmqEvent" body:@{@"name": @"connected"}];
-    }];
+        self.connection = [[RMQConnection alloc] initWithUri:uri channelMax:@65535 frameMax:@(RMQFrameMax) heartbeat:@10 connectTimeout:@15 readTimeout:@30 writeTimeout:@30 syncTimeout:@10 delegate:delegate delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
+        
+        [self.connection start:^{
+            self.channel = [self.connection createChannel];
+            [RnTinyRbmqEventEmitter emitEventWithName:@"RnTinyRbmqEvent" body:@{@"name": @"connected"}];
+        }];
+    }
 }
 
 RCT_EXPORT_METHOD(basicConsume:(NSString *) queue) {
-    self.consumer = [self.channel basicConsume:queue acknowledgementMode:(RMQBasicConsumeAcknowledgementModeAuto) handler:^(RMQMessage * _Nonnull message) {
-        NSString *body = [[NSString alloc] initWithData:message.body encoding:NSUTF8StringEncoding];
-               
-        [RnTinyRbmqEventEmitter emitEventWithName:@"RnTinyRbmqEvent"
-            body:@{
-                @"name": @"message",
-                @"queue_name": queue,
-                @"message": body,
-                @"routing_key": message.routingKey,
-                @"exchange": message.exchangeName,
-                @"consumer_tag": message.consumerTag,
-                @"delivery_tag": message.deliveryTag
-            }
-        ];
-    }];
+    if (self.isConnected) {
+        self.consumer = [self.channel basicConsume:queue acknowledgementMode:(RMQBasicConsumeAcknowledgementModeAuto) handler:^(RMQMessage * _Nonnull message) {
+            NSString *body = [[NSString alloc] initWithData:message.body encoding:NSUTF8StringEncoding];
+                   
+            [RnTinyRbmqEventEmitter emitEventWithName:@"RnTinyRbmqEvent"
+                body:@{
+                    @"name": @"message",
+                    @"queue_name": queue,
+                    @"message": body,
+                    @"routing_key": message.routingKey,
+                    @"exchange": message.exchangeName,
+                    @"consumer_tag": message.consumerTag,
+                    @"delivery_tag": message.deliveryTag
+                }
+            ];
+        }];
+    }
 }
 
 RCT_EXPORT_METHOD(close) {
-    [self.connection close];
-    self.connection = nil;
+    if (self.isConnected) {
+        [self.connection close];
+        self.connection = nil;
+        self.isConnected = false;
+    }
 }
 
 @end
